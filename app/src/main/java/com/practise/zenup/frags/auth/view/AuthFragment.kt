ea05beab2418ede301e129ce91c54ad4af887a39
Hariharan <hariakash1117@gmail.com>
2025-06-29 22:41:48 +0530

    Login and sign up function updated

--- app/src/main/java/com/practise/zenup/frags/auth/view/AuthFragment.kt
+++ app/src/main/java/com/practise/zenup/frags/auth/view/AuthFragment.kt
@@ -7,21 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.practise.zenup.R
+import com.practise.zenup.base.AppBaseFragment
 import com.practise.zenup.databinding.FragmentAuthBinding
+import com.practise.zenup.frags.auth.repo.AuthState
 import com.practise.zenup.frags.auth.viewmodel.AuthViewModel
+import com.practise.zenup.utils.isValidEmail
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AuthFragment : Fragment() {
+class AuthFragment : AppBaseFragment() {
 
     private val viewModel: AuthViewModel by viewModels()
     private lateinit var binding: FragmentAuthBinding
     private var signUpView : Boolean = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +35,20 @@ class AuthFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+
+            viewModel.authState.observe(viewLifecycleOwner) { state ->
+                authBtn.isEnabled = state != AuthState.Loading
+                when(state){
+                    AuthState.Success -> { findNavController().navigate(R.id.action_authFragment_to_homeFragment) }
+                    AuthState.Failed -> { showToast(getString(R.string.login_failed)) }
+                    else -> {}
+                }
+            }
+
+            authBtn.setOnClickListener {
+                if(signUpView) validateSignUp(emailFld.text.toString(), passwordFld.text.toString(), confirmPasswordFld.text.toString())
+                else validateSignIn(emailFld.text.toString(), passwordFld.text.toString())
+            }
 
             signUp.setOnClickListener { handleSignUp() }
 
@@ -57,4 +71,21 @@ class AuthFragment : Fragment() {
             }
         }
     }
+
+    private fun validateSignIn(email: String, password: String) {
+        binding.apply {
+            if(isValidEmail(email) && password.length >= 6){ viewModel.login(email, password) }
+            else if(!isValidEmail(email)){ emailFld.error = getString(R.string.invalid_email) }
+            else{ passwordFld.error = getString(R.string.invalid_password) }
+        }
+    }
+
+    private fun validateSignUp(email : String, password : String, confirmPassword: String) {
+        binding.apply {
+            if(isValidEmail(email) && password == confirmPassword){ viewModel.signup(email, password) }
+            else if(!isValidEmail(email)){ emailFld.error = getString(R.string.invalid_email) }
+            else{ confirmPasswordFld.error = getString(R.string.password_does_not_match) }
+        }
+    }
+
 }
--- app/src/main/java/com/practise/zenup/frags/auth/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/practise/zenup/frags/auth/viewmodel/AuthViewModel.kt
@@ -1,5 +1,32 @@
 package com.practise.zenup.frags.auth.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.practise.zenup.frags.auth.repo.AuthRepo
+import com.practise.zenup.frags.auth.repo.AuthState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AuthViewModel : ViewModel() {}
+
+@HiltViewModel
+class AuthViewModel @Inject constructor(private val authRepo: AuthRepo): ViewModel() {
+    private val _authState = MutableLiveData<AuthState>()
+    var authState : LiveData<AuthState> = _authState
+
+    fun login(email : String, password : String) {
+        viewModelScope.launch {
+            authRepo.login(email, password).collectLatest{ _authState.value = it }
+        }
+    }
+
+    fun signup(email : String, password : String) {
+        viewModelScope.launch {
+            authRepo.signup(email, password).collectLatest{ _authState.value = it }
+        }
+    }
+
+}
