7e0e7f0b162a144a80121aa1468b156d8f9dfeba
Hariharan <hariakash1117@gmail.com>
2025-06-29 16:38:36 +0530

    Auth view updated

--- app/src/main/java/com/practise/zenup/frags/auth/view/AuthFragment.kt
+++ app/src/main/java/com/practise/zenup/frags/auth/view/AuthFragment.kt
@@ -6,14 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.practise.zenup.R
 import com.practise.zenup.databinding.FragmentAuthBinding
 import com.practise.zenup.frags.auth.viewmodel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthFragment : Fragment() {
 
     private val viewModel: AuthViewModel by viewModels()
     private lateinit var binding: FragmentAuthBinding
+    private var signUpView : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +33,28 @@ class AuthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            signUp.setOnClickListener { handleSignUp() }
+
+        }
+
+    }
+
+    private fun handleSignUp() {
+        signUpView = !signUpView
+        binding.apply {
+            confirmPasswordFld.isVisible = signUpView
+            if(signUpView){
+                signUpInfo.setText(R.string.account_exits)
+                signUp.setText(R.string.login)
+                authBtn.setText(R.string.sign_up)
+            }else{
+                signUpInfo.setText(R.string.don_t_you_have_an_account)
+                signUp.setText(R.string.sign_up)
+                authBtn.setText(R.string.login)
+            }
+        }
     }
 }
