363061ece625e9ef234d47a6a506f0f6cf6a3656
Hariharan <hariakash1117@gmail.com>
2025-06-29 22:40:33 +0530

    di file updated

--- app/src/main/java/com/practise/zenup/di/ZenUpDI.kt
+++ app/src/main/java/com/practise/zenup/di/ZenUpDI.kt
@@ -1,10 +1,28 @@
 package com.practise.zenup.di
 
+import com.practise.zenup.frags.auth.repo.AuthRepo
+import com.practise.zenup.frags.auth.repo.AuthRepoImpl
+import com.practise.zenup.frags.splash.repo.SplashRepo
+import com.practise.zenup.frags.splash.repo.SplashRepoImpl
+import com.practise.zenup.model.FirebaseOps
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
-object ZenUpDI {}
+object ZenUpDI {
+
+    @Singleton @Provides
+    fun provideFireBaseOps() : FirebaseOps = FirebaseOps()
+
+    @Singleton @Provides
+    fun provideAuthRepo() : AuthRepo = AuthRepoImpl(provideFireBaseOps())
+
+    @Singleton @Provides
+    fun provideSplashRepo() : SplashRepo = SplashRepoImpl(provideFireBaseOps())
+
+}
