9ae50d83dc87907f442b6fa613710d8fcd79a171
Hariharan <hariakash1117@gmail.com>
2025-06-29 22:42:45 +0530

    Home view added

---
+++ app/src/main/java/com/practise/zenup/frags/home/view/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.practise.zenup.frags.home.view
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.practise.zenup.R
+import com.practise.zenup.frags.home.viewmodel.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
