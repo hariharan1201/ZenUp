f6a3a8cad1e2045ab6ba9b81a191a30c722f7ffe
Hariharan <hariakash1117@gmail.com>
2025-06-30 15:13:02 +0530

    base frag updated with progress loading

--- app/src/main/java/com/practise/zenup/base/AppBaseFrag.kt
+++ app/src/main/java/com/practise/zenup/base/AppBaseFrag.kt
@@ -1,5 +1,8 @@
 package com.practise.zenup.base
 
+import android.view.View
+
 interface AppBaseFrag {
     fun showToast(msg : String)
+    fun showProgressBar(show : Boolean)
 }
--- app/src/main/java/com/practise/zenup/base/AppBaseFragment.kt
+++ app/src/main/java/com/practise/zenup/base/AppBaseFragment.kt
@@ -1,10 +1,29 @@
 package com.practise.zenup.base
 
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.practise.zenup.R
 
 abstract class AppBaseFragment : Fragment(), AppBaseFrag {
+    private var loadingView: View? = null
+
     override fun showToast(msg : String){
         Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
     }
+
+    override fun showProgressBar(show: Boolean) {
+        val rootView = requireView() as ViewGroup
+        if (loadingView == null) {
+            val inflater = LayoutInflater.from(requireContext())
+            loadingView = inflater.inflate(R.layout.progress_loading, rootView, false)
+            rootView.addView(loadingView)
+        }
+        loadingView?.visibility = if (show) View.VISIBLE else View.GONE
+        Log.d("FIREBASE", "Progress bar visible: $show")
+    }
+
 }
