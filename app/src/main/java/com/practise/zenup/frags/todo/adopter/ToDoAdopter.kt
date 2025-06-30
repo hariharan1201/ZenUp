e7de60493cb5778307b67845bc47732471505cbf
Hariharan <hariakash1117@gmail.com>
2025-06-30 22:43:49 +0530

    oncheck ops added

--- app/src/main/java/com/practise/zenup/frags/todo/adopter/ToDoAdopter.kt
+++ app/src/main/java/com/practise/zenup/frags/todo/adopter/ToDoAdopter.kt
@@ -1,6 +1,5 @@
 package com.practise.zenup.frags.todo.adopter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.DocumentSnapshot
 import com.practise.zenup.R
 
-class ToDoAdopter(private val list: MutableList<DocumentSnapshot>) : RecyclerView.Adapter<ToDoAdopter.ToDoViewHolder>(){
+class ToDoAdopter(private val list: MutableList<DocumentSnapshot>, private val onItemCheck : (DocumentSnapshot) -> Unit)
+    : RecyclerView.Adapter<ToDoAdopter.ToDoViewHolder>(){
 
     inner class ToDoViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
         val username: AppCompatTextView = itemView.findViewById<AppCompatTextView>(R.id.name)
@@ -25,6 +25,7 @@ class ToDoAdopter(private val list: MutableList<DocumentSnapshot>) : RecyclerVie
     override fun onBindViewHolder(holder: ToDoAdopter.ToDoViewHolder, position: Int) {
         holder.username.text = list[position].get("ToDo").toString()
         holder.statusBox.isChecked = list[position].get("Status") as Boolean
+        holder.statusBox.setOnClickListener { onItemCheck(list[position]) }
     }
 
     override fun getItemCount(): Int = list.size
